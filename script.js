7a5f8b84a1b808de2f07237d3379821ee868035d
Chatorisbirthday <sahiljoshi339@gmail.com>
2026-08-03 16:15:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-function showMessage() {
-    document.getElementById("surprise").innerHTML =
-    "❤️ Happy Birthday! May your life always be full of smiles, success and beautiful memories. ❤️";
+function openSurprise() {
+
+    document.querySelector(".intro").style.display = "none";
+
+    document.getElementById("surpriseBox").style.display = "block";
+
+    createHearts();
+}
+
+
+function createHearts() {
+
+    for (let i = 0; i < 40; i++) {
+
+        let heart = document.createElement("div");
+
+        heart.innerHTML = "❤️";
+
+        heart.style.position = "fixed";
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.top = "100vh";
+        heart.style.fontSize = (Math.random() * 20 + 15) + "px";
+        heart.style.animation = "float 5s linear";
+
+        document.body.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+}
+
+@keyframes float {
+    from {
+        transform: translateY(0);
+        opacity: 1;
+    }
+
+    to {
+        transform: translateY(-120vh);
+        opacity: 0;
+    }
 }
